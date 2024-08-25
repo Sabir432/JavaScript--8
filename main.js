cee9e6283d84af4f750800fee1f36192f11e207e
Sabir <22800@gmail.com>
2024-08-25 11:58:18 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+let number = +prompt('Введите число')
+let power = +prompt('Введите степень')
+
+let result = 1
+
+for (let i = 0; i < power; i++) {
+  if (isNaN(number) && 0(number) || isNaN(power) && 0(power)) {
+    number = 2
+    power = 2
+  }
+
+  result *= number
+}
+
+alert(result)
